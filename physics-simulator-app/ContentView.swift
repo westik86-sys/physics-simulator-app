10a2d1693c37688757610f25a29a7fe6e8f94ffd
Westik86 <westik86@gmail.com>
2026-03-24 21:20:38 +0300

    Initial Commit

---
+++ physics-simulator-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  physics-simulator-app
+//
+//  Created by Павел Коростелев on 24.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ physics-simulator-app/physics_simulator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  physics_simulator_appApp.swift
+//  physics-simulator-app
+//
+//  Created by Павел Коростелев on 24.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct physics_simulator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
